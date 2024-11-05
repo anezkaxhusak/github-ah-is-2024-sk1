3f1efe850a4b955915394cd937c7e2b0b633e243
Anežka Husáková <anezka.husakova@tul.cz>
2024-11-05 21:39:58 +0100

    #dukol3

---
+++ Is-Projekty/dukol3/Program.cs
@@ -0,0 +1,21 @@
+﻿﻿class Program
+{
+    static void Main()
+    {
+        int size = 7;         
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+            
+                if (i == 0 || i == size - 1 || i == j )
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");                 }
+            }
+            Console.WriteLine();         }
+    }
+}
