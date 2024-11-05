dce99ec20250ca9066e673fed3107b625f72a1f9
Anežka Husáková <anezka.husakova@tul.cz>
2024-11-05 21:28:32 +0100

    #dukol

---
+++ ukol/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int size = 7;         
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+            
+                if (j == 0 || j == size - 1 || j == size - 1 - i )
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");                 }
+            }
+            Console.WriteLine();         }
+    }
+}
