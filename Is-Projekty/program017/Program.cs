af3b94f3ccb09a4ad76587353bedcf8cb909d93d
Anežka Husáková <anezka.husakova@tu.cz>
2024-12-04 10:16:29 +0100

    017

---
+++ Is-Projekty/program017/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Authentication.ExtendedProtection;
+using System.Security.Cryptography;
+using Microsoft.VisualBasic;
+
+string again = "a";
+        
+        while(again == "a") {
+            Console.Clear();
+    
+                
+
+
+
+            Console.WriteLine("***********************************");
+            Console.WriteLine("***** Fibonacciho Posloupnost *****");
+            Console.WriteLine("***********************************");
+            Console.WriteLine("***********************************");
+            Console.WriteLine("***********************************");
+            Console.WriteLine();
+
+        {
+            
+    
+        
+            Console.WriteLine();   
+            Console.WriteLine();
+            Console.WriteLine("Pro opakování programu stiskněte klávesu A");
+            Console.ReadLine();
+
+        }
+        }
+        
